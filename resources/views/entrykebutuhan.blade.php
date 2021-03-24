ab560d87bb03e5c118e5a54ccc13d2d0308e91f8
fuad al afif <fuadalafif19@gmail.com>
2021-03-24 09:31:56 +0700

    update tampilan entry kebutuhan pupuk ref #3

---
+++ resources/views/entrykebutuhan.blade.php
@@ -0,0 +1,68 @@
+<div class="main-content">
+    <!-- Header -->
+    <div class="container mt-5">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/jumb-1.jpg" width="50px" height="400px" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/jumb-2.jpg" width="50px" height="400px" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/jumb-3.jpg" width="50px" height="400px" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">no</th>
+      <th scope="col">pemohon</th>
+      <th scope="col">luas lahan</th>
+      <th scope="col">Urea</th>
+      <th scope="col">SP-36</th>
+      <th scope="col">NPK-Phonska</th>
+      <th scope="col">ZA</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>bagas</td>
+      <td>100m</td>
+      <td>10 kg</td>
+      <td>15 kg</td>
+      <td>20 kg</td>
+      <td>12 kg</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>bagus</td>
+      <td>100m</td>
+      <td>10 kg</td>
+      <td>15 kg</td>
+      <td>20 kg</td>
+      <td>12 kg</td>
+    </tr>
+    <tr>
+    <th scope="row">3</th>
+    <td>100m</td>
+      <td>10 kg</td>
+      <td>15 kg</td>
+      <td>20 kg</td>
+      <td>12 kg</td>
+    </tr>
+  </tbody>
+</table>
