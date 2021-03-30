8bf3c1eb48fd5fac5e1e0d1c770e710d9bd013f8
ekkychan <ekkychandra98@gmail.com>
2021-03-31 05:49:24 +0700

    create & update data petani resolved #7

--- app/Http/Controllers/PetaniController.php
+++ app/Http/Controllers/PetaniController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Distributor;
+use App\Models\Petani;
+use Brian2694\Toastr\Facades\Toastr;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PetaniController extends Controller
 {
@@ -10,7 +15,30 @@ class PetaniController extends Controller
         return view('petani.home');
     }
     public function data(){
-        return view('petani.addData');
+        $petani_id = Auth::user()->petani->id;
+        $petani = Petani::findOrFail($petani_id);
+        $distributors = Distributor::all();
+        return view('petani.addData', compact('distributors', 'petani'));
+    }
+    public function addPetani(Request $request){
+        $pupuk = Petani::updateOrCreate([
+                'user_id'=> Auth::user()->id
+            ],
+            [
+            'alamat' => $request->alamat,
+            'no_telp' => $request->no_hp,
+            'luas_tanah' => $request->luas_tanah,
+            'distributor_id'=> $request ->distributor_id,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        if($pupuk){
+            Toastr::success('data berhasil diinput', 'Sukses!');
+        }else{
+            Toastr::error('data gagal diinput', 'Gagal!');
+        }
+        return redirect()->route('petani.data');
     }
 }
 
