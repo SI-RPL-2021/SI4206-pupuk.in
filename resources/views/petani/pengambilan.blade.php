89a6f1116b23289b20d44a6564026f1c854cdeb8
MochRafiFawaji <71840179+MochRafiFawaji@users.noreply.github.com>
2021-05-24 21:46:31 +0700

    update tampilan pengambilan ref #12

---
+++ resources/views/petani/pengambilan.blade.php
@@ -0,0 +1,15 @@
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h3 class="mb-0">Form Pengambilan Pupuk</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>    
+</div>
