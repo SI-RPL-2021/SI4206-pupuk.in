c11de72751124de751d535d31224058b4be57727
MochRafiFawaji <71840179+MochRafiFawaji@users.noreply.github.com>
2021-05-24 20:33:32 +0700

    update tampilan tempat ref #12

---
+++ resources/views/petani/ambil.blade.php
@@ -0,0 +1,15 @@
+<div class="container mt-5">
+        <div class="row pt-3">
+            <div class="col-md-3 mt-3">
+                <div class="card h-100" style="">
+                    <a class="card-block stretched-link text-decoration-none" href="">
+                        <img class="card-img-top" src="{{asset('img/p_phonska.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title text-center">title</h4>
+                            <button type="button" class="btn btn-primary">title</button>
+                             
+                              </div>
+                        </div>
+                 </div>
+           </div>
+    </div>
